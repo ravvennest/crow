Traqueur-dev/PaperSpigot

CraftBlockEntityState mapping

paper-api-generator/src/main/java/io/papermc/generator/rewriter/types/simple/CraftBlockEntityStateMapping.java
@@ -1,0 +1,26 @@
+package io.papermc.generator.rewriter.types.simple;
+
+import io.papermc.generator.rewriter.SearchMetadata;
+import io.papermc.generator.rewriter.SearchReplaceRewriter;
+import io.papermc.generator.rewriter.types.Types;
+import io.papermc.generator.utils.BlockEntityMapping;
+import io.papermc.generator.utils.Formatting;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+
+public class CraftBlockEntityStateMapping extends SearchReplaceRewriter {
+
+    public CraftBlockEntityStateMapping(final String pattern) {
+        super(Types.CRAFT_BLOCKSTATES, pattern, false);
+    }
+
+    @Override
+    protected void insert(final SearchMetadata metadata, final StringBuilder builder) {
+        BlockEntityMapping.MAPPING.entrySet().stream().sorted(Formatting.alphabeticKeyOrder(entry -> entry.getKey().location().getPath())).forEach(entry -> {
+            builder.append(metadata.indent());
+            builder.append("register(%s.%s, %s.class, %s::new);".formatted(
+                BlockEntityType.class.getSimpleName(), Formatting.formatPathAsField(entry.getKey().location().getPath()),
+                entry.getValue(), entry.getValue()));
+            builder.append('\n');
+        });
+    }
+}

paper-api-generator/src/main/java/io/papermc/generator/utils/BlockEntityMapping.java
@@ -1,0 +1,34 @@
+package io.papermc.generator.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.google.common.base.CaseFormat;
+import com.google.common.collect.ImmutableMap;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+
+public final class BlockEntityMapping {
+
+    // if this become painful/too weird like the blockdata just rename the impl directly again
+    private static final Map<String, String> RENAMES = ImmutableMap.<String, String>builder()
+        .put("CraftFurnace", "CraftFurnaceFurnace")
+        .put("CraftMobSpawner", "CraftCreatureSpawner")
+        .put("CraftPiston", "CraftMovingPiston")
+        .put("CraftTrappedChest", "CraftChest") // not really a rename
+        .build();
+
+    public static final Map<ResourceKey<BlockEntityType<?>>, String> MAPPING;
+    static {
+        Map<ResourceKey<BlockEntityType<?>>, String> map = new HashMap<>();
+        BuiltInRegistries.BLOCK_ENTITY_TYPE.registryKeySet().forEach(key -> {
+            String name = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, key.location().getPath());
+            String implName = String.format("Craft%s", name);
+
+            map.put(key, RENAMES.getOrDefault(implName, implName));
+        });
+
+        MAPPING = Map.copyOf(map);
+    }
+
+}

ravvennest/crow

paper-api-generator/src/main/java/io/papermc/generator/rewriter/types/simple/CraftBlockEntityStateMapping.java
@@ -1,0 +1,26 @@
+package io.papermc.generator.rewriter.types.simple;
+
+import io.papermc.generator.rewriter.SearchMetadata;
+import io.papermc.generator.rewriter.SearchReplaceRewriter;
+import io.papermc.generator.rewriter.types.Types;
+import io.papermc.generator.utils.BlockEntityMapping;
+import io.papermc.generator.utils.Formatting;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+
+public class CraftBlockEntityStateMapping extends SearchReplaceRewriter {
+
+    public CraftBlockEntityStateMapping(final String pattern) {
+        super(Types.CRAFT_BLOCKSTATES, pattern, false);
+    }
+
+    @Override
+    protected void insert(final SearchMetadata metadata, final StringBuilder builder) {
+        BlockEntityMapping.MAPPING.entrySet().stream().sorted(Formatting.alphabeticKeyOrder(entry -> entry.getKey().location().getPath())).forEach(entry -> {
+            builder.append(metadata.indent());
+            builder.append("register(%s.%s, %s.class, %s::new);".formatted(
+                BlockEntityType.class.getSimpleName(), Formatting.formatKeyAsField(entry.getKey().location().getPath()),
+                entry.getValue(), entry.getValue()));
+            builder.append('\n');
+        });
+    }
+}

paper-api-generator/src/main/java/io/papermc/generator/utils/BlockEntityMapping.java
@@ -1,0 +1,35 @@
+package io.papermc.generator.utils;
+
+import java.util.Collections;
+import java.util.IdentityHashMap;
+import java.util.Map;
+import com.google.common.base.CaseFormat;
+import com.google.common.collect.ImmutableMap;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+
+public final class BlockEntityMapping {
+
+    // if this become painful/too weird like the blockdata just rename the impl directly again
+    private static final Map<String, String> RENAMES = ImmutableMap.<String, String>builder()
+        .put("CraftFurnace", "CraftFurnaceFurnace")
+        .put("CraftMobSpawner", "CraftCreatureSpawner")
+        .put("CraftPiston", "CraftMovingPiston")
+        .put("CraftTrappedChest", "CraftChest") // not really a rename
+        .build();
+
+    public static final Map<ResourceKey<BlockEntityType<?>>, String> MAPPING;
+    static {
+        Map<ResourceKey<BlockEntityType<?>>, String> map = new IdentityHashMap<>();
+        BuiltInRegistries.BLOCK_ENTITY_TYPE.registryKeySet().forEach(key -> {
+            String name = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, key.location().getPath());
+            String implName = String.format("Craft%s", name);
+
+            map.put(key, RENAMES.getOrDefault(implName, implName));
+        });
+
+        MAPPING = Collections.unmodifiableMap(map);
+    }
+
+}