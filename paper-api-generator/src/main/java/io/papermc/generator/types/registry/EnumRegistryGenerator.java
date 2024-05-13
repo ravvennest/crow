Lefiy/Paper

generate tags and structure type

paper-api-generator/src/main/java/io/papermc/generator/types/registry/EnumRegistryGenerator.java
@@ -1,0 +1,88 @@
+package io.papermc.generator.types.registry;
+
+import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterSpec;
+import com.squareup.javapoet.TypeSpec;
+import io.papermc.generator.Main;
+import io.papermc.generator.types.SimpleGenerator;
+import io.papermc.generator.utils.Annotations;
+import io.papermc.generator.utils.Formatting;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import javax.lang.model.element.Modifier;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import org.bukkit.Keyed;
+import org.bukkit.NamespacedKey;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.framework.qual.DefaultQualifier;
+
+@DefaultQualifier(NonNull.class)
+public abstract class EnumRegistryGenerator<T> extends SimpleGenerator {
+
+    protected final ResourceKey<Registry<T>> registryResourceKey;
+
+    public EnumRegistryGenerator(final String keysClassName, final String pkg, ResourceKey<Registry<T>> registryResourceKey) {
+        super(keysClassName, pkg);
+        this.registryResourceKey = registryResourceKey;
+    }
+
+    @Override
+    protected TypeSpec getTypeSpec() {
+        TypeSpec.Builder typeBuilder = TypeSpec.enumBuilder(this.className)
+            .addSuperinterface(Keyed.class)
+            .addModifiers(Modifier.PUBLIC)
+            .addAnnotations(Annotations.CLASS_HEADER);
+
+        Registry<T> event = Main.REGISTRY_ACCESS.registryOrThrow(this.registryResourceKey);
+        List<Map.Entry<ResourceKey<T>, T>> paths = new ArrayList<>(event.entrySet());
+        paths.sort(Comparator.comparing(o -> o.getKey().location().getPath()));
+
+        paths.forEach(entry -> {
+            ResourceKey<T> resourceKey = entry.getKey();
+            String pathKey = resourceKey.location().getPath();
+
+            String fieldName = Formatting.formatPathAsField(pathKey);
+            boolean isExperimental = this.isExperimental(entry);
+            TypeSpec.Builder builder = TypeSpec.anonymousClassBuilder("$S", pathKey);
+            if (isExperimental) {
+                builder.addAnnotations(Annotations.experimentalAnnotations(""));
+            }
+
+            typeBuilder.addEnumConstant(fieldName, builder.build());
+        });
+
+        FieldSpec keyField = FieldSpec.builder(NamespacedKey.class, "key", Modifier.PRIVATE).build();
+        typeBuilder.addField(keyField);
+
+        ParameterSpec keyParam = ParameterSpec.builder(String.class, "key").build();
+        typeBuilder.addMethod(MethodSpec.constructorBuilder()
+            .addParameter(keyParam).addCode("this.$N = $T.minecraft($N);", keyField, NamespacedKey.class, keyParam).build());
+
+        typeBuilder.addMethod(MethodSpec.methodBuilder("getKey")
+            .returns(NamespacedKey.class)
+            .addModifiers(Modifier.PUBLIC)
+            .addAnnotation(Annotations.NOT_NULL)
+            .addAnnotation(Annotations.OVERRIDE)
+            .addCode("return this.$N;", keyField).build());
+
+        this.addExtras(typeBuilder);
+
+        return typeBuilder.build();
+    }
+
+    public abstract void addExtras(TypeSpec.Builder builder);
+
+    @Override
+    protected JavaFile.Builder file(JavaFile.Builder builder) {
+        return builder
+            .skipJavaLangImports(true);
+    }
+
+    public abstract boolean isExperimental(Map.Entry<ResourceKey<T>, T> entry);
+
+}

paper-api-generator/src/main/java/io/papermc/generator/types/registry/StructureTypeGenerator.java
@@ -1,0 +1,92 @@
+package io.papermc.generator.types.registry;
+
+import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterSpec;
+import com.squareup.javapoet.TypeName;
+import com.squareup.javapoet.TypeSpec;
+import io.papermc.generator.types.SimpleGenerator;
+import io.papermc.generator.utils.Annotations;
+import io.papermc.generator.utils.Formatting;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import javax.lang.model.element.Modifier;
+import io.papermc.generator.utils.Javadocs;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceKey;
+import org.bukkit.Keyed;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
+import org.bukkit.generator.structure.StructureType;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.framework.qual.DefaultQualifier;
+
+import static com.squareup.javapoet.TypeSpec.classBuilder;
+import static io.papermc.generator.utils.Annotations.NOT_NULL;
+import static javax.lang.model.element.Modifier.FINAL;
+import static javax.lang.model.element.Modifier.PRIVATE;
+import static javax.lang.model.element.Modifier.PUBLIC;
+import static javax.lang.model.element.Modifier.STATIC;
+
+@DefaultQualifier(NonNull.class)
+public class StructureTypeGenerator extends SimpleGenerator { // todo abstract this one for similar stuff like game event?
+
+    private static final String CLASS_HEADER = Javadocs.getVersionDependentClassHeader("StructureTypes");
+
+    public StructureTypeGenerator(final String keysClassName, final String pkg) {
+        super(keysClassName, pkg);
+    }
+
+    private MethodSpec.Builder createMethod(TypeName returnType) {
+        final TypeName keyType = TypeName.get(String.class).annotated(NOT_NULL);
+
+        final ParameterSpec keyParam = ParameterSpec.builder(keyType, "key", FINAL).build();
+        final MethodSpec.Builder create = MethodSpec.methodBuilder("create")
+            .addModifiers(PRIVATE, STATIC)
+            .addParameter(keyParam)
+            .addCode("return $T.$L.get($T.minecraft($N));", Registry.class, "STRUCTURE_TYPE", NamespacedKey.class, keyParam)
+            .returns(returnType.annotated(NOT_NULL));
+        return create;
+    }
+
+    private TypeSpec.Builder keyHolderType() {
+        return classBuilder(this.className)
+            .addSuperinterface(Keyed.class)
+            .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
+            .addAnnotations(Annotations.CLASS_HEADER)
+            .addJavadoc(CLASS_HEADER);
+    }
+
+    @Override
+    protected TypeSpec getTypeSpec() {
+        TypeSpec.Builder typeBuilder = this.keyHolderType();
+        MethodSpec.Builder createMethod = this.createMethod(TypeName.get(StructureType.class));
+
+        List<Map.Entry<ResourceKey<net.minecraft.world.level.levelgen.structure.StructureType<?>>, net.minecraft.world.level.levelgen.structure.StructureType<?>>> paths = new ArrayList<>(BuiltInRegistries.STRUCTURE_TYPE.entrySet());
+        paths.sort(Comparator.comparing(o -> o.getKey().location().getPath()));
+
+        paths.forEach(entry -> {
+            ResourceKey<net.minecraft.world.level.levelgen.structure.StructureType<?>> resourceKey = entry.getKey();
+            String pathKey = resourceKey.location().getPath();
+            String fieldName = Formatting.formatPathAsField(pathKey);
+
+            FieldSpec.Builder fieldBuilder = FieldSpec.builder(StructureType.class, fieldName, PUBLIC, STATIC, FINAL)
+                .initializer("$N($S)", createMethod.build(), pathKey)
+                .addJavadoc(Javadocs.getVersionDependentField("{@code $L}"), resourceKey.location().toString());
+
+            typeBuilder.addField(fieldBuilder.build());
+        });
+
+        return typeBuilder.addMethod(createMethod.build()).build();
+    }
+
+    @Override
+    protected JavaFile.Builder file(JavaFile.Builder builder) {
+        return builder
+            .skipJavaLangImports(true);
+    }
+
+}

ravvennest/crow

paper-api-generator/src/main/java/io/papermc/generator/types/registry/EnumRegistryGenerator.java
@@ -1,0 +1,88 @@
+package io.papermc.generator.types.registry;
+
+import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterSpec;
+import com.squareup.javapoet.TypeSpec;
+import io.papermc.generator.Main;
+import io.papermc.generator.types.SimpleGenerator;
+import io.papermc.generator.utils.Annotations;
+import io.papermc.generator.utils.Formatting;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import javax.lang.model.element.Modifier;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import org.bukkit.Keyed;
+import org.bukkit.NamespacedKey;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.framework.qual.DefaultQualifier;
+
+@DefaultQualifier(NonNull.class)
+public abstract class EnumRegistryGenerator<T> extends SimpleGenerator {
+
+    protected final ResourceKey<Registry<T>> registryResourceKey;
+
+    public EnumRegistryGenerator(final String keysClassName, final String pkg, ResourceKey<Registry<T>> registryResourceKey) {
+        super(keysClassName, pkg);
+        this.registryResourceKey = registryResourceKey;
+    }
+
+    @Override
+    protected TypeSpec getTypeSpec() {
+        TypeSpec.Builder typeBuilder = TypeSpec.enumBuilder(this.className)
+            .addSuperinterface(Keyed.class)
+            .addModifiers(Modifier.PUBLIC)
+            .addAnnotations(Annotations.CLASS_HEADER);
+
+        Registry<T> event = Main.REGISTRY_ACCESS.registryOrThrow(this.registryResourceKey);
+        List<Map.Entry<ResourceKey<T>, T>> paths = new ArrayList<>(event.entrySet());
+        paths.sort(Comparator.comparing(o -> o.getKey().location().getPath()));
+
+        paths.forEach(entry -> {
+            ResourceKey<T> resourceKey = entry.getKey();
+            String pathKey = resourceKey.location().getPath();
+
+            String fieldName = Formatting.formatKeyAsField(pathKey);
+            boolean isExperimental = this.isExperimental(entry);
+            TypeSpec.Builder builder = TypeSpec.anonymousClassBuilder("$S", pathKey);
+            if (isExperimental) {
+                builder.addAnnotations(Annotations.experimentalAnnotations(null));
+            }
+
+            typeBuilder.addEnumConstant(fieldName, builder.build());
+        });
+
+        FieldSpec keyField = FieldSpec.builder(NamespacedKey.class, "key", Modifier.PRIVATE).build();
+        typeBuilder.addField(keyField);
+
+        ParameterSpec keyParam = ParameterSpec.builder(String.class, "key").build();
+        typeBuilder.addMethod(MethodSpec.constructorBuilder()
+            .addParameter(keyParam).addCode("this.$N = $T.minecraft($N);", keyField, NamespacedKey.class, keyParam).build());
+
+        typeBuilder.addMethod(MethodSpec.methodBuilder("getKey")
+            .returns(NamespacedKey.class)
+            .addModifiers(Modifier.PUBLIC)
+            .addAnnotation(Annotations.NOT_NULL)
+            .addAnnotation(Annotations.OVERRIDE)
+            .addCode("return this.$N;", keyField).build());
+
+        this.addExtras(typeBuilder);
+
+        return typeBuilder.build();
+    }
+
+    public abstract void addExtras(TypeSpec.Builder builder);
+
+    @Override
+    protected JavaFile.Builder file(JavaFile.Builder builder) {
+        return builder
+            .skipJavaLangImports(true);
+    }
+
+    public abstract boolean isExperimental(Map.Entry<ResourceKey<T>, T> entry);
+
+}

paper-api-generator/src/main/java/io/papermc/generator/types/registry/StructureTypeGenerator.java
@@ -1,0 +1,92 @@
+package io.papermc.generator.types.registry;
+
+import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterSpec;
+import com.squareup.javapoet.TypeName;
+import com.squareup.javapoet.TypeSpec;
+import io.papermc.generator.types.SimpleGenerator;
+import io.papermc.generator.utils.Annotations;
+import io.papermc.generator.utils.Formatting;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import javax.lang.model.element.Modifier;
+import io.papermc.generator.utils.Javadocs;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceKey;
+import org.bukkit.Keyed;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
+import org.bukkit.generator.structure.StructureType;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.framework.qual.DefaultQualifier;
+
+import static com.squareup.javapoet.TypeSpec.classBuilder;
+import static io.papermc.generator.utils.Annotations.NOT_NULL;
+import static javax.lang.model.element.Modifier.FINAL;
+import static javax.lang.model.element.Modifier.PRIVATE;
+import static javax.lang.model.element.Modifier.PUBLIC;
+import static javax.lang.model.element.Modifier.STATIC;
+
+@DefaultQualifier(NonNull.class)
+public class StructureTypeGenerator extends SimpleGenerator { // todo abstract this one for similar stuff like game event?
+
+    private static final String CLASS_HEADER = Javadocs.getVersionDependentClassHeader("StructureTypes");
+
+    public StructureTypeGenerator(final String keysClassName, final String pkg) {
+        super(keysClassName, pkg);
+    }
+
+    private MethodSpec.Builder createMethod(TypeName returnType) {
+        final TypeName keyType = TypeName.get(String.class).annotated(NOT_NULL);
+
+        final ParameterSpec keyParam = ParameterSpec.builder(keyType, "key", FINAL).build();
+        final MethodSpec.Builder create = MethodSpec.methodBuilder("create")
+            .addModifiers(PRIVATE, STATIC)
+            .addParameter(keyParam)
+            .addCode("return $T.$L.get($T.minecraft($N));", Registry.class, "STRUCTURE_TYPE", NamespacedKey.class, keyParam)
+            .returns(returnType.annotated(NOT_NULL));
+        return create;
+    }
+
+    private TypeSpec.Builder keyHolderType() {
+        return classBuilder(this.className)
+            .addSuperinterface(Keyed.class)
+            .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
+            .addAnnotations(Annotations.CLASS_HEADER)
+            .addJavadoc(CLASS_HEADER);
+    }
+
+    @Override
+    protected TypeSpec getTypeSpec() {
+        TypeSpec.Builder typeBuilder = this.keyHolderType();
+        MethodSpec.Builder createMethod = this.createMethod(TypeName.get(StructureType.class));
+
+        List<Map.Entry<ResourceKey<net.minecraft.world.level.levelgen.structure.StructureType<?>>, net.minecraft.world.level.levelgen.structure.StructureType<?>>> paths = new ArrayList<>(BuiltInRegistries.STRUCTURE_TYPE.entrySet());
+        paths.sort(Comparator.comparing(o -> o.getKey().location().getPath()));
+
+        paths.forEach(entry -> {
+            ResourceKey<net.minecraft.world.level.levelgen.structure.StructureType<?>> resourceKey = entry.getKey();
+            String pathKey = resourceKey.location().getPath();
+            String fieldName = Formatting.formatKeyAsField(pathKey);
+
+            FieldSpec.Builder fieldBuilder = FieldSpec.builder(StructureType.class, fieldName, PUBLIC, STATIC, FINAL)
+                .initializer("$N($S)", createMethod.build(), pathKey)
+                .addJavadoc(Javadocs.getVersionDependentField("{@code $L}"), resourceKey.location().toString());
+
+            typeBuilder.addField(fieldBuilder.build());
+        });
+
+        return typeBuilder.addMethod(createMethod.build()).build();
+    }
+
+    @Override
+    protected JavaFile.Builder file(JavaFile.Builder builder) {
+        return builder
+            .skipJavaLangImports(true);
+    }
+
+}