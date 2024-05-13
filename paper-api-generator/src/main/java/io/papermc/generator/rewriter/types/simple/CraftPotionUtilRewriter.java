Traqueur-dev/PaperSpigot

potion data and stats bridge

paper-api-generator/src/main/java/io/papermc/generator/rewriter/types/simple/CraftPotionUtilRewriter.java
@@ -1,0 +1,38 @@
+package io.papermc.generator.rewriter.types.simple;
+
+import io.papermc.generator.rewriter.SearchMetadata;
+import io.papermc.generator.rewriter.SearchReplaceRewriter;
+import io.papermc.generator.rewriter.types.Types;
+import io.papermc.generator.utils.Formatting;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import org.bukkit.potion.PotionType;
+import java.util.Locale;
+
+@Deprecated(forRemoval = true)
+public class CraftPotionUtilRewriter extends SearchReplaceRewriter {
+
+    private final String statePrefix;
+
+    public CraftPotionUtilRewriter(final String pattern, String statePrefix) {
+        super(Types.CRAFT_POTION_UTIL, pattern, false);
+        this.statePrefix = statePrefix;
+    }
+
+    @Override
+    protected void insert(SearchMetadata metadata, StringBuilder builder) {
+        String upperStatePrefix = statePrefix.toUpperCase(Locale.ENGLISH);
+        BuiltInRegistries.POTION.holders()
+            .filter(reference -> {
+                ResourceLocation location = reference.key().location();
+                return BuiltInRegistries.POTION.containsKey(new ResourceLocation(location.getNamespace(), this.statePrefix + "_" + location.getPath()));
+            })
+            .sorted(Formatting.alphabeticKeyOrder(reference -> reference.key().location().getPath())).forEach(reference -> {
+                String internalName = Formatting.formatPathAsField(reference.key().location().getPath());
+                builder.append(metadata.indent());
+                builder.append(".put(%s.%s, %s.%s_%s)".formatted(PotionType.class.getSimpleName(), PotionTypeRewriter.FIELD_RENAMES.getOrDefault(internalName, internalName), PotionType.class.getSimpleName(), upperStatePrefix, internalName));
+                builder.append('\n');
+            });
+    }
+
+}

paper-api-generator/src/main/java/io/papermc/generator/rewriter/types/simple/StatisticRewriter.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 import io.papermc.generator.Main;
 import io.papermc.generator.rewriter.types.EnumRegistryRewriter;
+import io.papermc.generator.rewriter.types.Types;
 import io.papermc.generator.utils.ClassHelper;
 import io.papermc.generator.utils.Formatting;
 import net.minecraft.core.Holder;
@@ -19,6 +20,8 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import org.bukkit.Statistic;
+
+import static io.papermc.generator.utils.Formatting.quoted;
 
 @Deprecated(forRemoval = true)
 public class StatisticRewriter {
@@ -67,6 +70,29 @@ public class StatisticRewriter {
         protected String rewriteEnumName(Holder.Reference<ResourceLocation> reference) {
             String internalName = super.rewriteEnumName(reference);
             return FIELD_RENAMES.getOrDefault(internalName, internalName);
+        }
+    }
+
+    public static class CraftCustom extends EnumRegistryRewriter<ResourceLocation, Statistic> {
+
+        private static Map<String, String> INTERNAL_FIELD_RENAMES = Map.of(
+            "SNEAK_TIME", "CROUCH_TIME"
+        );
+
+        public CraftCustom(final String pattern) {
+            super(Types.CRAFT_STATISTIC, Registries.CUSTOM_STAT, pattern, true);
+        }
+
+        @Override
+        protected String rewriteEnumName(Holder.Reference<ResourceLocation> reference) {
+            String internalName = super.rewriteEnumName(reference);
+            return FIELD_RENAMES.getOrDefault(internalName, internalName);
+        }
+
+        @Override
+        protected String rewriteEnumValue(Holder.Reference<ResourceLocation> reference) {
+            String internalName = Formatting.formatPathAsField(reference.key().location().getPath());
+            return "%s.%s".formatted(Stats.class.getSimpleName(), INTERNAL_FIELD_RENAMES.getOrDefault(internalName, internalName));
         }
     }
 
@@ -123,6 +149,30 @@ public class StatisticRewriter {
                 throw new IllegalStateException("Unable to translate stat type generic " + genericType.getCanonicalName() + " into the api!");
             }
             return "%s.%s".formatted(Statistic.Type.class.getSimpleName(), TYPE_MAPPING.get(genericType)); // find a more direct way?
+        }
+    }
+
+    public static class CraftType extends EnumRegistryRewriter<StatType<?>, Statistic> {
+
+        public CraftType(final String pattern) {
+            super(Types.CRAFT_STATISTIC, Registries.STAT_TYPE, pattern, true);
+        }
+
+        @Override
+        protected Iterable<Holder.Reference<StatType<?>>> getValues() {
+            return Main.REGISTRY_ACCESS.registryOrThrow(Registries.STAT_TYPE).holders().filter(reference -> reference.value() != Stats.CUSTOM)
+                .sorted(Formatting.alphabeticKeyOrder(reference -> reference.key().location().getPath())).toList();
+        }
+
+        @Override
+        protected String rewriteEnumName(Holder.Reference<StatType<?>> reference) {
+            String internalName = super.rewriteEnumName(reference);
+            return FIELD_RENAMES.getOrDefault(internalName, internalName);
+        }
+
+        @Override
+        protected String rewriteEnumValue(Holder.Reference<StatType<?>> reference) {
+            return "new %s(%s)".formatted(ResourceLocation.class.getSimpleName(), quoted(reference.key().location().getPath()));
         }
     }
 }

ravvennest/crow

paper-api-generator/src/main/java/io/papermc/generator/rewriter/types/simple/CraftPotionUtilRewriter.java
@@ -1,0 +1,38 @@
+package io.papermc.generator.rewriter.types.simple;
+
+import io.papermc.generator.rewriter.SearchMetadata;
+import io.papermc.generator.rewriter.SearchReplaceRewriter;
+import io.papermc.generator.rewriter.types.Types;
+import io.papermc.generator.utils.Formatting;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import org.bukkit.potion.PotionType;
+import java.util.Locale;
+
+@Deprecated(forRemoval = true)
+public class CraftPotionUtilRewriter extends SearchReplaceRewriter {
+
+    private final String statePrefix;
+
+    public CraftPotionUtilRewriter(final String pattern, String statePrefix) {
+        super(Types.CRAFT_POTION_UTIL, pattern, false);
+        this.statePrefix = statePrefix;
+    }
+
+    @Override
+    protected void insert(SearchMetadata metadata, StringBuilder builder) {
+        String upperStatePrefix = statePrefix.toUpperCase(Locale.ENGLISH);
+        BuiltInRegistries.POTION.holders()
+            .filter(reference -> {
+                ResourceLocation location = reference.key().location();
+                return BuiltInRegistries.POTION.containsKey(new ResourceLocation(location.getNamespace(), this.statePrefix + "_" + location.getPath()));
+            })
+            .sorted(Formatting.alphabeticKeyOrder(reference -> reference.key().location().getPath())).forEach(reference -> {
+                String internalName = Formatting.formatKeyAsField(reference.key().location().getPath());
+                builder.append(metadata.indent());
+                builder.append(".put(%s.%s, %s.%s_%s)".formatted(PotionType.class.getSimpleName(), internalName, PotionType.class.getSimpleName(), upperStatePrefix, internalName));
+                builder.append('\n');
+            });
+    }
+
+}

paper-api-generator/src/main/java/io/papermc/generator/rewriter/types/simple/StatisticRewriter.java
@@ -9,6 +9,7 @@ import java.util.IdentityHashMap;
 import java.util.Map;
 import io.papermc.generator.Main;
 import io.papermc.generator.rewriter.types.EnumRegistryRewriter;
+import io.papermc.generator.rewriter.types.Types;
 import io.papermc.generator.utils.ClassHelper;
 import io.papermc.generator.utils.Formatting;
 import net.minecraft.core.Holder;
@@ -20,6 +21,8 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import org.bukkit.Statistic;
+
+import static io.papermc.generator.utils.Formatting.quoted;
 
 @Deprecated(forRemoval = true)
 public class StatisticRewriter {
@@ -68,6 +71,29 @@ public class StatisticRewriter {
         protected String rewriteEnumName(Holder.Reference<ResourceLocation> reference) {
             String internalName = super.rewriteEnumName(reference);
             return FIELD_RENAMES.getOrDefault(internalName, internalName);
+        }
+    }
+
+    public static class CraftCustom extends EnumRegistryRewriter<ResourceLocation, Statistic> {
+
+        private static final Map<String, String> INTERNAL_FIELD_RENAMES = Map.of(
+            "SNEAK_TIME", "CROUCH_TIME"
+        );
+
+        public CraftCustom(final String pattern) {
+            super(Types.CRAFT_STATISTIC, Registries.CUSTOM_STAT, pattern, true);
+        }
+
+        @Override
+        protected String rewriteEnumName(Holder.Reference<ResourceLocation> reference) {
+            String internalName = super.rewriteEnumName(reference);
+            return FIELD_RENAMES.getOrDefault(internalName, internalName);
+        }
+
+        @Override
+        protected String rewriteEnumValue(Holder.Reference<ResourceLocation> reference) {
+            String internalName = Formatting.formatKeyAsField(reference.key().location().getPath());
+            return "%s.%s".formatted(Stats.class.getSimpleName(), INTERNAL_FIELD_RENAMES.getOrDefault(internalName, internalName));
         }
     }
 
@@ -124,6 +150,30 @@ public class StatisticRewriter {
                 throw new IllegalStateException("Unable to translate stat type generic " + genericType.getCanonicalName() + " into the api!");
             }
             return "%s.%s".formatted(Statistic.Type.class.getSimpleName(), TYPE_MAPPING.get(genericType)); // find a more direct way?
+        }
+    }
+
+    public static class CraftType extends EnumRegistryRewriter<StatType<?>, Statistic> {
+
+        public CraftType(final String pattern) {
+            super(Types.CRAFT_STATISTIC, Registries.STAT_TYPE, pattern, true);
+        }
+
+        @Override
+        protected Iterable<Holder.Reference<StatType<?>>> getValues() {
+            return Main.REGISTRY_ACCESS.registryOrThrow(Registries.STAT_TYPE).holders().filter(reference -> reference.value() != Stats.CUSTOM)
+                .sorted(Formatting.alphabeticKeyOrder(reference -> reference.key().location().getPath())).toList();
+        }
+
+        @Override
+        protected String rewriteEnumName(Holder.Reference<StatType<?>> reference) {
+            String internalName = super.rewriteEnumName(reference);
+            return FIELD_RENAMES.getOrDefault(internalName, internalName);
+        }
+
+        @Override
+        protected String rewriteEnumValue(Holder.Reference<StatType<?>> reference) {
+            return "new %s(%s)".formatted(ResourceLocation.class.getSimpleName(), quoted(reference.key().location().getPath()));
         }
     }
 }