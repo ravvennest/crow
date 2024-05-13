Traqueur-dev/PaperSpigot

composite rewriter

paper-api-generator/src/main/java/io/papermc/generator/rewriter/types/RegistryFieldRewriter.java
@@ -76,8 +76,7 @@ public class RegistryFieldRewriter<T, A> extends SearchReplaceRewriter {
 
     @Override
     protected void insert(final SearchMetadata metadata, final StringBuilder builder) {
-        List<Holder.Reference<T>> references = this.registry.holders().sorted(Formatting.alphabeticOrder(reference -> reference.key().location().getPath())).toList();
-        Iterator<Holder.Reference<T>> referenceIterator = references.iterator();
+        Iterator<Holder.Reference<T>> referenceIterator = this.registry.holders().sorted(Formatting.alphabeticOrder(reference -> reference.key().location().getPath())).iterator();
 
         while (referenceIterator.hasNext()) {
             Holder.Reference<T> reference = referenceIterator.next();

paper-api-generator/src/main/java/io/papermc/generator/rewriter/types/TagRewriter.java
@@ -58,8 +58,7 @@ public class TagRewriter extends SearchReplaceRewriter {
             builder.append('\n');
             builder.append('\n');
 
-            List<? extends TagKey<?>> tagKeys = registry.getTagNames().sorted(Formatting.alphabeticOrder(tagKey -> tagKey.location().getPath())).toList();
-            Iterator<? extends TagKey<?>> keyIterator = tagKeys.iterator();
+            Iterator<? extends TagKey<?>> keyIterator = registry.getTagNames().sorted(Formatting.alphabeticOrder(tagKey -> tagKey.location().getPath())).iterator();
 
             while (keyIterator.hasNext()) {
                 TagKey<?> tagKey = keyIterator.next();

ravvennest/crow

paper-api-generator/src/main/java/io/papermc/generator/rewriter/types/RegistryFieldRewriter.java
@@ -77,8 +77,7 @@ public class RegistryFieldRewriter<T, A> extends SearchReplaceRewriter {
 
     @Override
     protected void insert(final SearchMetadata metadata, final StringBuilder builder) {
-        List<Holder.Reference<T>> references = this.registry.holders().sorted(Formatting.alphabeticKeyOrder(reference -> reference.key().location().getPath())).toList();
-        Iterator<Holder.Reference<T>> referenceIterator = references.iterator();
+        Iterator<Holder.Reference<T>> referenceIterator = this.registry.holders().sorted(Formatting.alphabeticKeyOrder(reference -> reference.key().location().getPath())).iterator();
 
         while (referenceIterator.hasNext()) {
             Holder.Reference<T> reference = referenceIterator.next();

paper-api-generator/src/main/java/io/papermc/generator/rewriter/types/TagRewriter.java
@@ -58,8 +58,7 @@ public class TagRewriter extends SearchReplaceRewriter {
             builder.append('\n');
             builder.append('\n');
 
-            List<? extends TagKey<?>> tagKeys = registry.getTagNames().sorted(Formatting.alphabeticKeyOrder(tagKey -> tagKey.location().getPath())).toList();
-            Iterator<? extends TagKey<?>> keyIterator = tagKeys.iterator();
+            Iterator<? extends TagKey<?>> keyIterator = registry.getTagNames().sorted(Formatting.alphabeticKeyOrder(tagKey -> tagKey.location().getPath())).iterator();
 
             while (keyIterator.hasNext()) {
                 TagKey<?> tagKey = keyIterator.next();